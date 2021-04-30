091ea44cc11c2603776e34c34a66cb185ce51daa
jballou2 <47549964+jballou2@users.noreply.github.com>
2021-04-30 18:31:17 -0400

    Add files via upload

---
+++ hello.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#1 
+echo "Welcome to computer science society."
+#2
+#Jordon Ballou
+#jordon Ballou
+#jballou1@student.gsu.edu
+#3
+echo "Date is = `date`"
+#4
+echo */ | wc
+#5
+echo $PATH
+echo $USER
+echo $SHELL
+#6
+echo "Printing disk usage"
+echo "`df`"
+#7
+echo "Could you laon me "$"25.00?"
+#8
+echo "if x =2, x*x = 4, x/2 = 1"
+#9
+echo "`ls *.sh | grep c`"
+#10
+echo "Good Bye"
+echo "Current Hour = `date +"%H"`"
+
+#end script
+
---
+++ myName.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+  
+int main(void)
+{
+printf("My name is Jordon Ballou");
+return 0;
+}
